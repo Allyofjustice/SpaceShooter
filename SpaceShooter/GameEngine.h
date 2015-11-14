e9d05a68fed4b253badf451052a0f7bc24f503c3
patrik <patrik_swe@hotmail.com>
2015-11-14 23:46:13 +0100

    Refactoring for game states

--- SpaceShooter/GameEngine.h
+++ SpaceShooter/GameEngine.h
@@ -12,57 +12,30 @@
 #include <sstream>
 #include "MyTimer.h"
 #include <SDL_mixer.h>
+#include "StartScreen.h"
 
 class GameEngine{
 public:
-	GameEngine(int width, int height, std::vector<Sprite*> gameObjects, Player *player1);
+	GameEngine(int width, int height);
 	void run();
-	bool getQuit();
 	
 private:
 	const int FPS = 60; 
 	const int TICKS_PER_FRAME = 1000 / FPS;
-
 	double currentFPS;
+	int width;
+	int height;
+	SDL_Surface* gameScreen; //= SDL_SetVideoMode(width, height, 32, SDL_SWSURFACE); //640, 480, 32 SDL_SWSURFACE
+	GameState *currentState; //Stores the current state of the game
 
+	//functions
+	void change_state(); 
 	void cleanup();
 	void init();
-	void removeDead();
-	void sort();
-	void checkCollisions();
 	void draw();
 	void update();
 	void pause();
 	void handleInput();
-	void mainMenu();
-
-	std::vector<Sprite*> gameObjects;
-	std::vector<SolidSprite*> collisionObjects;
-	std::vector<Sprite*> noCollisionObjects;
-	int width;
-	int height;
-	int playerHealth = 100;
-	SDL_Surface* gameScreen; //= SDL_SetVideoMode(width, height, 32, SDL_SWSURFACE); //640, 480, 32 SDL_SWSURFACE
-	SDL_Surface *message;
-	SDL_Surface *oldMessage;
-
-	TTF_Font *font;
-	SDL_Event event;
-	Player* player1;
-	SDL_Rect offset;
-	SDL_Color textColor;
-
-	Mix_Music *music = NULL;
-
-	int score;
-	int scoreTimerGap = 10000;
-
-	std::stringstream strScore;
-	std::stringstream strLife;
-
-	bool quit = false;
-	bool gameOver;
-	
 	
 };
 #endif
